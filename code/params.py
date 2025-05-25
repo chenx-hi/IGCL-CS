66a8467b69982dd5f21c851e96b3432afe701d6c
Xiang Chen <53766284+chenx-hi@users.noreply.github.com>
2025-05-25 11:13:58 +0800

    Update params.py

--- code/params.py
+++ code/params.py
@@ -379,53 +379,7 @@ def texas_params():
     ## basic info
     parser.add_argument('--dataset_name', type=str, default=dataset)
     parser.add_argument('--k_hop', type=int, default=0)
-    parser.add_argument('--cluster', type=float, default=0.03) #0.02
-    parser.add_argument('--gpu', type=int, default=0)
-    parser.add_argument('--seed', type=int, default=1024) #1024
-    parser.add_argument('--epochs', type=int, default=50)
-    parser.add_argument('--cls_epochs', type=int, default=1000)
-
-    parser.add_argument('--rand_split_class', type=bool, default=True,
-                        help='use random splits with a fixed number of labeled nodes for each class')
-    parser.add_argument('--rand_split', type=bool, default=False)
-    parser.add_argument('--label_num_per_class', type=int, default=20,
-                        help='labeled nodes per class(randomly selected)')
-    parser.add_argument('--valid_num', type=int, default=500,
-                        help='Total number of validation')
-    parser.add_argument('--test_num', type=int, default=1000,
-                        help='Total number of test')
-    parser.add_argument('--train_ratio', type=float, default=0.5,
-                        help='training label proportion')
-    parser.add_argument('--valid_ratio', type=float, default=0.25,
-                        help='validation label proportion')
-
-    ## mlp
-    parser.add_argument('--hidden_channels', type=int, default=2048) #4096
-    parser.add_argument('--mlp_layers', type=int, default=1)
-    parser.add_argument('--proj_layers', type=int, default=2)
-    parser.add_argument('--tau', type=float, default=0.2)
-    parser.add_argument('--beta', type=float, default=0.97)
-    parser.add_argument('--lamda', type=float, default=0)
-    parser.add_argument('--batch_norm', type=bool, default=False)
-    parser.add_argument('--layer_norm', type=bool, default=False)
-
-    ## optimizer
-    parser.add_argument('--lr', type=float, default=0.0001)
-    parser.add_argument('--weight_decay', type=float, default=0.005)
-
-    parser.add_argument('--cls_lr', type=float, default=0.1)
-    parser.add_argument('--cls_weight_decay', type=float, default=0.005)
-    #####################################
-    args, _ = parser.parse_known_args()
-    return args
-
-def wisconsin_params():
-    parser = argparse.ArgumentParser()
-    #####################################
-    ## basic info
-    parser.add_argument('--dataset_name', type=str, default=dataset)
-    parser.add_argument('--k_hop', type=int, default=0)
-    parser.add_argument('--cluster', type=float, default=0.02)
+    parser.add_argument('--cluster', type=float, default=0.05) #0.05
     parser.add_argument('--gpu', type=int, default=0)
     parser.add_argument('--seed', type=int, default=1024) #1024
     parser.add_argument('--epochs', type=int, default=100)
@@ -440,38 +394,85 @@ def wisconsin_params():
                         help='Total number of validation')
     parser.add_argument('--test_num', type=int, default=1000,
                         help='Total number of test')
-    parser.add_argument('--train_ratio', type=float, default=0.5,
+    parser.add_argument('--train_ratio', type=float, default=0.6,
                         help='training label proportion')
-    parser.add_argument('--valid_ratio', type=float, default=0.25,
+    parser.add_argument('--valid_ratio', type=float, default=0.2,
                         help='validation label proportion')
 
     ## mlp
-    parser.add_argument('--hidden_channels', type=int, default=2048)
+    parser.add_argument('--hidden_channels', type=int, default=4096) #4096
     parser.add_argument('--mlp_layers', type=int, default=1)
-    parser.add_argument('--proj_layers', type=int, default=1)
-    parser.add_argument('--tau', type=float, default=0.65)
-    parser.add_argument('--beta', type=float, default=0.9)
+    parser.add_argument('--proj_layers', type=int, default=2)
+    parser.add_argument('--tau', type=float, default=0.8)
+    parser.add_argument('--beta', type=float, default=0.97)
+    parser.add_argument('--lamda', type=float, default=1)
+    parser.add_argument('--batch_norm', type=bool, default=False)
+    parser.add_argument('--layer_norm', type=bool, default=False)
+
+    ## optimizer
+    parser.add_argument('--lr', type=float, default=0.0005)
+    parser.add_argument('--weight_decay', type=float, default=0.01)
+
+    parser.add_argument('--cls_lr', type=float, default=0.1)
+    parser.add_argument('--cls_weight_decay', type=float, default=0.0001)
+    #####################################
+    args, _ = parser.parse_known_args()
+    return args
+
+
+def wisconsin_params():
+    parser = argparse.ArgumentParser()
+    #####################################
+    ## basic info
+    parser.add_argument('--dataset_name', type=str, default=dataset)
+    parser.add_argument('--k_hop', type=int, default=0)
+    parser.add_argument('--cluster', type=float, default=0.07) #0.07
+    parser.add_argument('--gpu', type=int, default=0)
+    parser.add_argument('--seed', type=int, default=1024) #1024
+    parser.add_argument('--epochs', type=int, default=25)
+    parser.add_argument('--cls_epochs', type=int, default=600)
+
+    parser.add_argument('--rand_split_class', type=bool, default=True,
+                        help='use random splits with a fixed number of labeled nodes for each class')
+    parser.add_argument('--rand_split', type=bool, default=False)
+    parser.add_argument('--label_num_per_class', type=int, default=20,
+                        help='labeled nodes per class(randomly selected)')
+    parser.add_argument('--valid_num', type=int, default=500,
+                        help='Total number of validation')
+    parser.add_argument('--test_num', type=int, default=1000,
+                        help='Total number of test')
+    parser.add_argument('--train_ratio', type=float, default=0.6,
+                        help='training label proportion')
+    parser.add_argument('--valid_ratio', type=float, default=0.2,
+                        help='validation label proportion')
+
+    ## mlp
+    parser.add_argument('--hidden_channels', type=int, default=4096) #4096
+    parser.add_argument('--mlp_layers', type=int, default=1)
+    parser.add_argument('--proj_layers', type=int, default=2)
+    parser.add_argument('--tau', type=float, default=1)
+    parser.add_argument('--beta', type=float, default=0.97)
     parser.add_argument('--lamda', type=float, default=0.9)
     parser.add_argument('--batch_norm', type=bool, default=True)
     parser.add_argument('--layer_norm', type=bool, default=False)
 
     ## optimizer
     parser.add_argument('--lr', type=float, default=0.001)
-    parser.add_argument('--weight_decay', type=float, default=0.0001)
+    parser.add_argument('--weight_decay', type=float, default=0.05)
 
     parser.add_argument('--cls_lr', type=float, default=0.01)
-    parser.add_argument('--cls_weight_decay', type=float, default=0.05)
+    parser.add_argument('--cls_weight_decay', type=float, default=0.0005)
     #####################################
     args, _ = parser.parse_known_args()
     return args
 
-def chameleon_params():
+def cornell_params():
     parser = argparse.ArgumentParser()
     #####################################
     ## basic info
     parser.add_argument('--dataset_name', type=str, default=dataset)
-    parser.add_argument('--k_hop', type=int, default=1)
-    parser.add_argument('--cluster', type=float, default=0.01) #0.01
+    parser.add_argument('--k_hop', type=int, default=0)
+    parser.add_argument('--cluster', type=float, default=0.06) #0.06
     parser.add_argument('--gpu', type=int, default=0)
     parser.add_argument('--seed', type=int, default=1024) #1024
     parser.add_argument('--epochs', type=int, default=150)
@@ -486,27 +487,27 @@ def chameleon_params():
                         help='Total number of validation')
     parser.add_argument('--test_num', type=int, default=1000,
                         help='Total number of test')
-    parser.add_argument('--train_ratio', type=float, default=0.5,
+    parser.add_argument('--train_ratio', type=float, default=0.6,
                         help='training label proportion')
-    parser.add_argument('--valid_ratio', type=float, default=0.25,
+    parser.add_argument('--valid_ratio', type=float, default=0.2,
                         help='validation label proportion')
 
     ## mlp
-    parser.add_argument('--hidden_channels', type=int, default=2048)
+    parser.add_argument('--hidden_channels', type=int, default=2048) #512
     parser.add_argument('--mlp_layers', type=int, default=1)
-    parser.add_argument('--proj_layers', type=int, default=2)
-    parser.add_argument('--tau', type=float, default=1)
-    parser.add_argument('--beta', type=float, default=0.95)
-    parser.add_argument('--lamda', type=float, default=0.2)
+    parser.add_argument('--proj_layers', type=int, default=1)
+    parser.add_argument('--tau', type=float, default=0.3)
+    parser.add_argument('--beta', type=float, default=0.97)
+    parser.add_argument('--lamda', type=float, default=0.9)
     parser.add_argument('--batch_norm', type=bool, default=False)
-    parser.add_argument('--layer_norm', type=bool, default=True)
+    parser.add_argument('--layer_norm', type=bool, default=False)
 
     ## optimizer
-    parser.add_argument('--lr', type=float, default=0.005)
-    parser.add_argument('--weight_decay', type=float, default=0.00005)
+    parser.add_argument('--lr', type=float, default=0.0005)
+    parser.add_argument('--weight_decay', type=float, default=0.1)
 
-    parser.add_argument('--cls_lr', type=float, default=0.05)
-    parser.add_argument('--cls_weight_decay', type=float, default=0.005)
+    parser.add_argument('--cls_lr', type=float, default=0.1)
+    parser.add_argument('--cls_weight_decay', type=float, default=0.000005)
     #####################################
     args, _ = parser.parse_known_args()
     return args
@@ -532,9 +533,9 @@ def actor_params():
                         help='Total number of validation')
     parser.add_argument('--test_num', type=int, default=1000,
                         help='Total number of test')
-    parser.add_argument('--train_ratio', type=float, default=0.5,
+    parser.add_argument('--train_ratio', type=float, default=0.6,
                         help='training label proportion')
-    parser.add_argument('--valid_ratio', type=float, default=0.25,
+    parser.add_argument('--valid_ratio', type=float, default=0.2,
                         help='validation label proportion')
 
     ## mlp
